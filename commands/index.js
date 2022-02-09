580558dd7b4024c2c8ee52add374646536cca8ed
LaureiVarju <mdilzell@yahoo.com>
2022-02-09 15:59:15 -0500

    added !keys functionality and some mykey updates

--- commands/index.js
+++ commands/index.js
@@ -2,6 +2,6 @@ module.exports = {
   Ping: require('./ping'),
   Joke: require('./joke'),
   Key: require('./keys'),
-  MyKey: require('./mykey'),
+  MyKey: require('./mykeys'),
   GetTime: require('./gettime'),
 }
